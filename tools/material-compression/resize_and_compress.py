b0919a58ce3244b08844f72c8d7616a9427f366f
Redox <mail@redox.to>
2024-06-05 18:05:05 +0200

    Add crash handler and extra info

--- tools/material-compression/resize_and_compress.py
+++ tools/material-compression/resize_and_compress.py
@@ -1,27 +1,53 @@
 import os
+import signal
 from resizelib import resizeVTF
 
 # Edit these variables
 PATH_TO_DIR = r"garrysmod\addons\addon_name\materials"
-CLAMP_SIZE = 1024
+CLAMP_SIZE = 512  # This can be set to a absurdly high number to only use DXT1 compression, or a lower number to clamp the resolution to that size.
 # End of variables
 
 old_size = 0
 new_size = 0
 replace_count = 0
 
+
+def signal_handler(sig, frame):
+    if os.path.exists("crashfile.txt"):
+        os.remove("crashfile.txt")
+
+    print("Cancelled by user.")
+    exit()
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+if os.path.exists("crashfile.txt"):
+    with open("crashfile.txt", "r") as f:
+        print("Crash/exit detected! Last file processed:", f.read())
+        print("Try importing and exporting the VTF with VTFEdit to fix it.")
+
+    os.remove("crashfile.txt")
+
 for path, subdirs, files in os.walk(PATH_TO_DIR):
     for name in files:
         if not name.endswith(".vtf"):
             continue
 
-        old_size_temp = os.path.getsize(path)
+        with open("crashfile.txt", "w") as f:
+            f.write(os.path.join(path, name))
+
+        old_size_temp = os.path.getsize(os.path.join(path, name))
         converted = resizeVTF(os.path.join(path, name), CLAMP_SIZE)
         if converted:
             replace_count += 1
-            new_size += os.path.getsize(path)
+            new_size += os.path.getsize(os.path.join(path, name))
+            old_size += old_size_temp
+        else:
+            new_size += old_size_temp
             old_size += old_size_temp
 
+os.remove("crashfile.txt")
 print("Replaced", replace_count, "files.")
 if replace_count == 0:
     print("No files were replaced.")
